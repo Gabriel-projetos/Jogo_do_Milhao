45822f93840d111c86b08c6e4f2e411a4ffa3a68
Paula Marukawa <paulamarukawa@alunos.utfpr.edu.br>
2025-06-22 22:30:12 -0300

    Arrumando o código

--- Headers/jogo.h
+++ Headers/jogo.h
@@ -6,5 +6,7 @@
 Pergunta* sorteiaPergunta(Pergunta *perguntas, int total);
 void embaralhaPerguntas(Pergunta **originais, Pergunta **embaralhadas, int total);
 Pergunta* sorteiaPorNivel(Pergunta *perguntas, int total, int nivel);
+void jogoAcontece(Pergunta *perguntas, int total); 
+void perguntaDoMilhao(Pergunta *perguntas); 
 
-#endif
+#endif // JOGO_H
